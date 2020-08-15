4b680798d7393e272cff3d5d1607cde14784d88e
153091 <xpoint06@gmail.com>
2020-08-15 21:05:03 +0300

    WordNet constructor

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,7 +1,73 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
 public class WordNet {
 
+    private HashMap<Integer, SET<String>> stIdString = new HashMap<>();
+    private HashMap<String, SET<Integer>> stStringId = new HashMap<>();
+    private SAP sap;
+    private Digraph graph;
+
     // constructor takes the name of the two input files
-    public WordNet(String synsets, String hypernyms)
+    public WordNet(String synsets, String hypernyms) {
+        if ((synsets == null) || (hypernyms == null)) {
+            throw new IllegalArgumentException("input argument is null");
+        }
+
+
+        // structure of synsets
+        try {
+            In synsetsIn = new In(synsets);
+            while (!synsetsIn.isEmpty()) {
+                String s = synsetsIn.readLine();
+                String[] fields = s.split(",");
+                int id = Integer.parseInt(fields[0]);
+                SET<String> set = new SET<>();
+                String[] words = fields[1].split(" ");
+                for (int i = 0; i < words.length; i++) {
+                    set.add(words[i]);
+
+                    SET<Integer> ids;
+                    if (!stStringId.containsKey(words[i])) {
+                        ids = new SET<>();
+                    }
+                    else {
+                        ids = stStringId.get(words[i]);
+                    }
+                    ids.add(id);
+                    stStringId.put(words[i], ids);
+                }
+                stIdString.put(id, set);
+            }
+        }
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in WordNet constructor", e);
+        }
+        // digraph constructor
+        int vertices = stIdString.size();
+        graph = new Digraph(vertices);
+
+        // structure for hypernyms
+        try {
+            In hypernymsIn = new In(hypernyms);
+            while (!hypernymsIn.isEmpty()) {
+                String s = hypernymsIn.readLine();
+                String[] fields = s.split(",");
+                int id = Integer.parseInt(fields[0]);
+                for (int i = 1; i < fields.length; i++) {
+                    int w = Integer.parseInt(fields[i]);
+                    graph.addEdge(id, w);
+                }
+            }
+        }
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in WordNet constructor", e);
+        }
+    }
 
     // returns all WordNet nouns
     public Iterable<String> nouns()
